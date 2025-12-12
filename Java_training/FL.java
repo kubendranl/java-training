3dd6f6185ff67c490434dde53eeefce537976b3e
KUBENDRAN L <kubendran529@gmail.com>
2025-12-12 09:07:02 +0530

    Add files via upload

---
+++ Java_training/FL.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class FL {
+    public static void main(String[] args) {
+
+       Scanner sc= new Scanner(System.in);
+       int a = sc.nextInt();
+       
+       System.out.println(a/100);
+       System.out.println(a%10);
+       
+}
+}
---
+++ Java_training/Swapwith.java
@@ -0,0 +1,19 @@
+
+    import java.util.Scanner;
+public class Swapwith{
+    public static void main(String[] args) {
+
+       Scanner sc= new Scanner(System.in);
+       int a = sc.nextInt();
+       int b = sc.nextInt();
+      a=a+b;//20
+      b=a-b;//10
+      a=a-b;//10
+       System.out.println(a);
+        System.out.println(b);
+
+}
+
+}
+
+
