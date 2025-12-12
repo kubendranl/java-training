bd1808e7774a7e90ad231bf20d964774b070e9a9
KUBENDRAN L <kubendran529@gmail.com>
2025-12-12 09:05:53 +0530

    Delete Java_training directory

--- Java_training/Mess.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Mess {
-     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        String a = sc.nextLine();
-        System.out.println("Enter the Message : "+ a);
-}
-}
--- Java_training/deci.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class deci {
-    public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        Double a = sc.nextDouble();
-     System.out.println(a);
-}
-}
