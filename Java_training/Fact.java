0754bb6e13f1862d41e6267431e2d9d80d7edd7b
KUBENDRAN L <kubendran529@gmail.com>
2025-12-13 13:55:49 +0530

    Delete Java_training directory

--- Java_training/Fact.java
+++
@@ -1,9 +0,0 @@
- import java.util.Scanner;
-public class Fact {
-     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        Float a = sc.nextFloat();
-        System.out.println(a);
-}
-}
-
